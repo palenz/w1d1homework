c9dfaa257563662ef3593bf7de54f06e37b820a0
Juan <jpalenzuela@outlook.com>
2020-11-16 23:05:10 +0000

    wee game with names

---
+++ precourse_recap.py
@@ -0,0 +1,12 @@
+name = input('What is your name? \n(pd: this game runs better if your name is Jack, Juan, Lucy or Alex). \n \nanswer: ')
+
+if name == 'Jack':
+    print('Jack the one who hacks')
+elif name == 'Juan':
+    print('Juan the stubborn one')
+elif name == 'Lucy':
+    print('Lucy doesn`t like sushi')
+elif name == 'Alex':
+    print('Alex lives in a palace')
+else:
+    print('You might want to reconsider changing your name to Jack, Juan, Lucy or Alex so you can play this game')
